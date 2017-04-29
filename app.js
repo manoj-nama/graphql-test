9f5f339b5191f7e12c506040010b5ad300d242dd
Manoj Nama <manoj.nama@outlook.com>
2017-04-29 15:25:47 +0530

    Adding schema defs structure for graphql

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const exGraphQL = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-
 const config = require('./config');
+
+const PORT = process.env.PORT || 8888;
 
 const schema = buildSchema(`
   type Query {
@@ -12,17 +13,22 @@ const schema = buildSchema(`
 `);
 
 const rootValue = {
-  hello: () => null//'Hello World!'
+  hello: () => 'Hello World!'
 }
 
 const app = express();
 
 mongoose.connect(config.mongo_uri);
 
-app.use('/', exGraphQL({
+app.use('/graphql', exGraphQL({
   schema,
   rootValue,
   graphiql: true
 }));
 
-app.listen(8888);
+app.listen(PORT, () => {
+  console.log(`
+    Server listening on http://localhost:${PORT}/
+    The GraphiQL interface is available on http://localhost:${PORT}/graphql
+  `);
+});
